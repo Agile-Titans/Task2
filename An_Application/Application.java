85edc844e53c12a3509b44ed11f574ad64a3c8dc
Elham💕 <elhaamoha20@gmail.com>
2026-03-14 10:34:59 +0300

    Update Application.java

--- An_Application/Application.java
+++ An_Application/Application.java
@@ -1,36 +1,78 @@
-//sprint 1 - Frame & Basid
 import javax.swing.*;
 import java.awt.*;
 
 public class Application {
-    public static void main(String[] args){
-        JFrame frame = new JFrame();
+    JFrame frame;
+    JLabel label;
+    JButton button;
+    JPanel panel;
+    JTextArea textArea;
+    JButton button1, button2, button3;
+    JScrollPane scrollPane;
+
+
+    public Application() {
+        this.CreateJFrame();
+    }
+
+    public JFrame CreateJFrame(){
+        frame = new JFrame();
         frame.setTitle("BorderLayout Demo");
         frame.setSize(600, 400);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JLabel label = new JLabel("Application Title");
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        frame.add(label, BorderLayout.NORTH);
-
-        JButton button = new JButton();
-        button.setText("Submit");
-        frame.add(button, BorderLayout.SOUTH);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 1));
-        JButton button1 = new JButton("Option1");
-        JButton button2 = new JButton("Option2");
-        JButton button3 = new JButton("Option3");
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
-        frame.add(panel, BorderLayout.WEST);
-
-        JTextArea textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        frame.add(scrollPane, BorderLayout.CENTER);
-
+        frame.add(this.CreateJLabel(), BorderLayout.NORTH);
+        frame.add(this.CreateJButton(), BorderLayout.SOUTH);
+        frame.add(this.CreateJPanel(), BorderLayout.WEST);
+        frame.add(this.CreateJTextArea());
+        frame.add(this.CreateJScrollPane(), BorderLayout.CENTER);
+        frame.setVisible(true);
+        return frame;
     }
+
+    public JLabel CreateJLabel(){
+        label = new JLabel("Application Title");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        return label;
+    }
+
+    public JButton CreateJButton(){
+        button = new JButton();
+        button.setText("Submit");
+        return button;
+    }
+
+    public JButton Createbutton1(){
+        button1 = new JButton("Option1");
+        return button1;
+    }
+
+    public JButton Createbutton2(){
+        button2 = new JButton("Option2");
+        return button2;
+    }
+
+    public JButton Createbutton3(){
+        button3 = new JButton("Option3");
+        return button3;
+    }
+
+    public JPanel CreateJPanel(){
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 1));
+        panel.add(this.Createbutton1());
+        panel.add(this.Createbutton2());
+        panel.add(this.Createbutton3());
+        return panel;
+    }
+
+    public JScrollPane CreateJScrollPane(){
+        scrollPane = new JScrollPane(textArea);
+        return scrollPane;
+    }
+
+    public JTextArea CreateJTextArea(){
+        textArea = new JTextArea();
+        return textArea;
+    }
+
 }
