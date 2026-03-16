8de342c56aef5d0741cf35a6bb47d2b411126651
Elham💕 <elhaamoha20@gmail.com>
2026-03-16 11:23:05 +0300

    commit

---
+++ Test/ApplicationTest.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createJFrame() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createJLabel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createJButton() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createbutton1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createbutton2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createbutton3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createJPanel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createJScrollPane() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createJTextArea() {
+    }
+}
